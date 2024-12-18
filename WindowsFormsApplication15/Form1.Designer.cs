59cedb2429a48b8e7bd00c0ad56bc2b0d7ac9bf8
xzyqiu <xzyqiu@gmail.com>
2024-12-18 21:18:25 +0300

    Kodlar

---
+++ WindowsFormsApplication15/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApplication15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBoxDepartman = new System.Windows.Forms.ComboBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSatisTutari = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 168);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 24;
+            // 
+            // comboBoxDepartman
+            // 
+            this.comboBoxDepartman.FormattingEnabled = true;
+            this.comboBoxDepartman.Location = new System.Drawing.Point(88, 86);
+            this.comboBoxDepartman.Name = "comboBoxDepartman";
+            this.comboBoxDepartman.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxDepartman.TabIndex = 23;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 139);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 22;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Toplam Tutar";
+            // 
+            // txtSatisTutari
+            // 
+            this.txtSatisTutari.Location = new System.Drawing.Point(88, 113);
+            this.txtSatisTutari.Name = "txtSatisTutari";
+            this.txtSatisTutari.Size = new System.Drawing.Size(100, 20);
+            this.txtSatisTutari.TabIndex = 20;
+            this.txtSatisTutari.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Departman";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Maaş";
+            // 
+            // txtMaas
+            // 
+            this.txtMaas.Location = new System.Drawing.Point(88, 61);
+            this.txtMaas.Name = "txtMaas";
+            this.txtMaas.Size = new System.Drawing.Size(100, 20);
+            this.txtMaas.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(88, 35);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Numara";
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(88, 9);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(100, 20);
+            this.txtNumara.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 277);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBoxDepartman);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSatisTutari);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMaas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumara);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBoxDepartman;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSatisTutari;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMaas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumara;
+    }
+}
+
---
+++ WindowsFormsApplication15/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication15
+{
+    public partial class Form1 : Form
+    {
+        // Çalışanlar listesini tutacak
+        List<Personel> personelListesi = new List<Personel>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Departmanları ComboBox'a ekliyoruz
+            comboBoxDepartman.Items.Add("Otomobil Satışı");
+            comboBoxDepartman.Items.Add("Yedek Parça Satışı");
+            comboBoxDepartman.SelectedIndex = 0; // İlk seçeneği varsayılan olarak seç
+        }
+
+        // Ekle butonuna tıklanınca çalışacak metod
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numara = int.Parse(txtNumara.Text);
+                string adSoyad = txtAdSoyad.Text;
+                decimal maas = decimal.Parse(txtMaas.Text);
+                decimal satisTutari = decimal.Parse(txtSatisTutari.Text);
+                string departman = comboBoxDepartman.SelectedItem.ToString();
+                Personel yeniPersonel;
+
+                if (departman == "Otomobil Satışı")
+                {
+                    yeniPersonel = new OtomobilSatisi(numara, adSoyad, maas, departman, satisTutari);
+                }
+                else
+                {
+                    yeniPersonel = new YedekParcaSatisi(numara, adSoyad, maas, departman, satisTutari);
+                }
+
+                personelListesi.Add(yeniPersonel);
+
+                listBox1.Items.Clear();
+                foreach (var personel in personelListesi)
+                {
+                    listBox1.Items.Add("Numara: " + personel.Numarası + "- Ad Soyad: " + personel.AdSoyad + "- Maaş: " + personel.Maas + "- Satış Tutarı: " + personel.SatisTutari + "- Departman: " + personel.Departman);
+                }
+
+                txtNumara.Clear();
+                txtAdSoyad.Clear();
+                txtMaas.Clear();
+                txtSatisTutari.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veri girişi hatası: " + ex.Message);
+            }
+        }
+    }
+
+    public class Personel
+    {
+        public int Numarası { get; set; }
+        public string AdSoyad { get; set; }
+        public decimal Maas { get; set; }
+        public string Departman { get; set; }
+        public decimal SatisTutari { get; set; }
+
+        public Personel(int numara, string adSoyad, decimal maas, string departman, decimal satisTutari)
+        {
+            Numarası = numara;
+            AdSoyad = adSoyad;
+            Maas = maas;
+            Departman = departman;
+            SatisTutari = satisTutari;
+        }
+    }
+
+    public class OtomobilSatisi : Personel
+    {
+        public OtomobilSatisi(int numara, string adSoyad, decimal maas, string departman, decimal satisTutari)
+            : base(numara, adSoyad, maas, departman, satisTutari)
+        {
+        }
+    }
+
+    public class YedekParcaSatisi : Personel
+    {
+        public YedekParcaSatisi(int numara, string adSoyad, decimal maas, string departman, decimal satisTutari)
+            : base(numara, adSoyad, maas, departman, satisTutari)
+        {
+        }
+    }
+}
